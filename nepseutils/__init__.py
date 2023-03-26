58301a05f2dc80458274bbfa7ac18c38f6fa24ff
Daze <dazehere@yandex.com>
2023-03-26 09:38:14 +0545

    bump: Version bump to 0.3.5

--- nepseutils/__init__.py
+++ nepseutils/__init__.py
@@ -1,3 +1,3 @@
 from nepseutils.meroshare import MeroShare
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
--- tests/test_nepseutils.py
+++ tests/test_nepseutils.py
@@ -2,4 +2,4 @@ from nepseutils import __version__
 
 
 def test_version():
-    assert __version__ == "0.3.4"
+    assert __version__ == "0.3.5"
