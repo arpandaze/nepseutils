a965a51c3e5e2d62aac6c5f683e515af377b4b74
Daze <dazehere@yandex.com>
2023-03-26 09:26:11 +0545

    feat: share type and group is now displayed while applying

--- nepseutils/__main__.py
+++ nepseutils/__main__.py
@@ -239,12 +239,14 @@ class NepseUtils(Cmd):
 
             if not company_to_apply:
                 appicable_issues = ms.get_applicable_issues()
-                headers = ["Share ID", "Company Name", "Scrip", "Close Date"]
+                headers = ["Share ID", "Company Name", "Scrip", "Type", "Group","Close Date"]
                 table = [
                     [
                         itm.get("companyShareId"),
                         itm.get("companyName"),
                         itm.get("scrip"),
+                        itm.get("shareTypeName"),
+                        itm.get("shareGroupName"),
                         itm.get("issueCloseDate"),
                     ]
                     for itm in appicable_issues
